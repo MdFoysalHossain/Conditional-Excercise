152c290544dbb679de7b573a4ad3acb5ea347656
Foysal Hossain <74594464+MdFoysalHossain@users.noreply.github.com>
2025-07-27 22:59:16 +0600

    3rd Paractice

---
+++ Practice3.js
@@ -0,0 +1,13 @@
+var mark = 60;
+
+if(mark >= 90 && mark <= 100 ){
+    console.log("You Got A")
+} else if(mark >= 80 && mark <= 89 ){
+    console.log("You Got B")
+} else if(mark >= 70 && mark <= 79 ){
+    console.log("You Got C")
+}else if(mark >= 60 && mark <= 69 ){
+    console.log("You Got D")
+}else if(mark >= 0 && mark <= 59 ){
+    console.log("You Got F")
+}

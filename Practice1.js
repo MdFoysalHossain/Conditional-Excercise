d07aa2b78b85a5e0b803fb8f698d3b332eb15766
Foysal Hossain <74594464+MdFoysalHossain@users.noreply.github.com>
2025-07-27 22:43:14 +0600

    Practice 1

---
+++ Practice1.js
@@ -0,0 +1,8 @@
+var amountBought = 400;
+var coke = 30;
+
+if(amountBought > 500){
+    console.log("Burger and Free Coke");
+} else{
+    console.log("Burger: ", amountBought, "\nCoke Added: ", coke, "\nTotal Amount: ", amountBought+coke)
+}
